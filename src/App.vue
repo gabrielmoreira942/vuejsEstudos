52b8e7c30d6a92063c72aca571340682395e25fa
Gabriel Moreira <maboroshiken@gmail.com>
2021-06-10 17:16:22 -0300

    Armazenando resultados de pesquisa  em outra variável

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,53 @@
 <template>
-  <div id="app">OLÁ KKKKKK
-  <div class="container grid-xs py-2">OLÁ KKKKKK
-  <img class=" img-responsive img-totoro" src="@/assets/totoro.jpg" alt="testes com totoro">
-  <form>
-      <div class="input-group">
-        <input type="text" class="form-input" placeholder="Spirited Away...">
-        <button class="btn btn-primary input-group-btn">Pesquisar filme</button>
-      </div>
-  </form>
-  </div>
+  <div id="app">
+    OLÁ KKKKKK
+    <div class="container grid-xs py-2">
+      OLÁ KKKKKK
+      <img
+        class="img-responsive img-totoro"
+        src="@/assets/totoro.jpg"
+        alt="Filtro com totoro"
+      />
+      <form @submit.prevent="recebePesquisa(pesquisa)">
+        <div class="input-group">
+          <input
+            type="text"
+            v-model="pesquisa.teste"
+            class="form-input"
+            placeholder="Spirited Away..."
+          />
+          <button class="btn btn-primary input-group-btn">
+            Pesquisar filme
+          </button>
+        </div>
+        {{ pesquisas }}
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+  data() {
+  return { 
+    pesquisas: [], 
+    pesquisa: {checked: false} 
+    
+    };
+  },
+  methods: {
+    recebePesquisa(pesquisa) {
+      pesquisa.id = Date.now()
+      this.pesquisas.push(pesquisa)
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .img-totoro {
   max-width: 200px;
   margin: 0 auto;
 }
-
 </style>
